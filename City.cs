6d11b5b89f81a48836a94bcbf4856a34fd160c7c
İsmail Barış Yaraş <102619217+Barisyaras@users.noreply.github.com>
2022-04-16 07:50:00 +0300

    Add files via upload

---
+++ City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.UI
+{
+    public class City
+    {
+        public double Fahrenheit { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ UserManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.UI
+{
+    public class UserManager
+    {
+        public List<City> cityList = new List<City> { new City() };
+        public void Add(City city)
+        {
+                cityList.Add(city);
+        }
+
+        public List<City> GetList() 
+        {
+            return cityList;
+        }
+    }
+}
