fbdd03a892b4c9fbb44550bf1e99ae0d1df8d7f0
barisyaras <ismayaras@hotmail.com>
2022-04-17 17:56:58 +0300

    usermaneger was deleted.

---
+++ Weather.UI/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.UI
+{
+    public class City
+    {
+        public double Fahrenheit { get; set; }
+        public string Name { get; set; }
+        public double Celcius { get; set; }
+    }
+}
--- Weather.UI/Program.cs
+++ Weather.UI/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Weather.UI;
+
+List<City> cityList = new List<City>();
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Eklemek istediğiniz şehiri yazınız");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Eklemek istediğiniz şehrin sıcaklığını celcius cinsinden yazınız");
+    double celcius = Convert.ToInt32(Console.ReadLine());
+    double fahrenheit = celcius * 1.8 + 32;
+
+    City city = new City();
+    city.Name = name;
+    city.Fahrenheit = fahrenheit;
+    city.Celcius = celcius; 
+
+    cityList.Add(city);
+
+}
+
+
+int length = cityList.Count;
+for (int i = 0; i < length; i++)
+{
+
+    Console.WriteLine(cityList[i].Name+" "+cityList[i].Fahrenheit+"Fahrenheit "+cityList[i].Celcius+"Celcius");
+}
